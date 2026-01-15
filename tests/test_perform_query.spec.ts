2a88639c9d32a8bd29994b22079f5f9e08fc2a50
Nicolas Salcedo <nssalnic@gmail.com>
2026-01-15 15:00:04 -0500

    restructured tests dir for POM with pages , specs , fixutres and utils folders added

--- tests/test_perform_query.spec.ts
+++ tests/test_perform_query.spec.ts
@@ -1,5 +1,20 @@
 import { expect, test } from '@playwright/test';
     
+type character = {
+    name: string;   
+    grade: '4' | '5';
+    element: 'Hydro' | 'Pyro' | 'Anemo' | 'Electro' | 'Dendro' | 'Cryo' | 'Geo';
+    weapon: 'Sword' | 'Claymore' | 'Polearm' | 'Bow' | 'Catalyst';
+};
+
+test('should search for top 5 Genshin Impact characters and verify results', async ({ page }) => {
+    // Navigate to Genshin Impact Wiki
+    await page.goto('https://genshin-impact.fandom.com/wiki/Genshin_Impact_Wiki');
+    await page.getByRole('link', { name: 'Characters' }).nth(1).click();
+
+});
+
+
 test('should search for top 5 soccer teams and verify results', async ({ page }) => {
             // Navigate to Google
             await page.goto('https://serebii.net/');
@@ -10,3 +25,34 @@ test('should search for top 5 soccer teams and verify results', async ({ page })
     
             
  });
+
+
+
+ test('Search Runescape - Name of Fruit trees and their details',async ({page})=>{
+    const searchWord = "fruit trees" 
+    var data = null;
+    var table;
+    //go to runescape wiki
+     await page.goto("https://runescape.wiki/");
+     await expect(page.getByRole('combobox',{name:'Search'})).toBeVisible();
+  // search for fruit seeds
+    await page.getByRole('combobox',{name:'Search'}).fill(searchWord);
+    await page.getByRole('button',{
+        name:'search'}).click()
+
+   
+  //retrive Details 
+  await expect(page.locator('span').filter({hasText: searchWord}).first()).toBeVisible();
+  // validate Table is visible 
+   table  = await page.locator("//table[7]");
+    expect(table,"Fruit seed table not visible and found").toBeVisible();
+  //store them into JSON format
+data = {level: , seed: , protection:,
+    growthTime: , experience:{
+        plant: ,checkhealth: , harvest: 
+    }, cost:,fruit: ,ProduceUses:
+   }
+  // build table with information
+
+ });
+
